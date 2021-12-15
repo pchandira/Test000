14a5be28c756bfa4264eb7b4b62b6249fdf320b3
Chandiran <chandira@ebizoncloud.com>
2021-12-15 09:58:29 +0530

    Signed-off-by: Chandiran <chandira@ebizoncloud.com>

---
+++ env_var.sh
@@ -0,0 +1,10 @@
+export TF_VAR_tenancy_ocid=""
+export TF_VAR_user_ocid=""
+export TF_VAR_region=us-ashburn-1
+export TF_VAR_compartment_ocid=""
+# proivde the private subnet OCID
+export TF_VAR_subnet_ocid="ocid1.subnet.oc1.iad.aaaaaaaaebjnlhyutu6vzxlrfzraxmy7cotqalx7lmgthgbgpdurveq24ciq"
+export TF_VAR_fingerprint=""
+export TF_VAR_private_key_path="${HOME}/.oci/oci_api_key.pem"
+# provide your SSH Public Key
+export TF_VAR_ssh_public_keys="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYnx57dpXDrPpXfNiYjC5PkJSG5U7b18sVVzj7b4qRl7zgvKquhFPkmHAH2QqoQqGxoHrCmLtPrcVLjZer93B8m8aEF+p7ANHEWh4Mg4o9EcZWOD5EuUR7Kdlzo5V/20RbaOYjUQ5hp1brPMn6OvAJETfUW5Gac+EcPscYlCyY7UYKWSOfgsQXeXH/Q7yTMTyL4ishS1i5cXmWtv7MeuxC+4BZOMcsZMWTa5kkpkCPa4v36/gEFC+/KvQ5Fd0rl+oal0TLUIVnVeNdUBEAR5nlw+7Se5H45LvhnTHRIHyUGRnCKCDdRwJ5+tONsGfO+j1ZFjl+n/QNuxBPnXkGcYGR ssh-key-2021-09-16"
